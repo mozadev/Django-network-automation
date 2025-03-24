176e68068b453d58e46e61a41db1fb8f9ee2218c
mozadev <ceosmore@gmail.com>
2025-03-24 14:40:56 -0500

    Implement 'create()' method in 'CreateInforme' class

--- scripts/apis/rest/modules/create_informe/utils.py
+++ scripts/apis/rest/modules/create_informe/utils.py
@@ -6,3 +6,16 @@ class CreateInforme:
         self.data = data
         self.name = f"media/informes/{now}.docx"
 
+    def create(self):
+        doc = DocxTemplate(self.template)
+       
+        self.context = {
+            "titulo": self.data["titulo"],
+            "cliente": self.data["cliente"],
+            "reportes": self.apply_clean_format(self.data["reportes"]) 
+
+        }
+        doc.render(self.context)
+        doc.save(self.name)
+        return self.name
+
